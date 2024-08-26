19054cd44c1c011df5145110113b667f7348161a
Olga <ya1994ng@gmail.com>
2024-08-26 18:01:09 +0800

    feat: completed all case except boundary case

--- StakingPointsSystemTests/ScoreCalculatorTests.cs
+++ StakingPointsSystemTests/ScoreCalculatorTests.cs
@@ -27,9 +27,9 @@ public class ScoreCalculatorTests
         var userId = 1;
         _mockContext.Users.AddRange(new User { UserId = userId });
         await _mockContext.SaveChangesAsync();
-        
+
         await _scoreCalculator.Calculate(GetTime(59, 50));
-        
+
         var userScore = _mockContext.UserScores.SingleOrDefault(x => x.UserId == userId);
         userScore.Should().BeNull();
     }
@@ -52,11 +52,9 @@ public class ScoreCalculatorTests
             }
         });
         _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Banana, Unit = 4 });
-
         await _mockContext.SaveChangesAsync();
 
         await _scoreCalculator.Calculate(GetTime(59, 50));
-
 
         var userScore = _mockContext.UserScores.Single(x => x.UserId == userId);
         userScore.TotalScore.Should().Be(40 * 20 * 4); // second * based score * unit 
@@ -91,6 +89,154 @@ public class ScoreCalculatorTests
 
         var userScore = _mockContext.UserScores.Single(x => x.UserId == userId);
         userScore.TotalScore.Should().Be(100 + 20 * 20 * 4); //previous score + second * based score * unit 
+    }
+
+    [Test]
+    public async Task has_deposit_after_last_update()
+    {
+        var userId = 1;
+        _mockContext.Users.AddRange(new User { UserId = userId });
+        _mockContext.UserScores.Add(new UserScore
+        {
+            UserId = userId,
+            TotalScore = 100,
+            LastUpdatedTime = GetTime(59, 10)
+        });
+        _mockContext.Assets.AddRange(new List<Asset>()
+        {
+            new()
+            {
+                UserId = userId,
+                TransactionType = TransactionType.Deposit,
+                AssetType = AssetType.Banana,
+                Unit = 4,
+                CreatedTime = GetTime(59, 40)
+            }
+        });
+        _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Banana, Unit = 6 });
+
+        await _mockContext.SaveChangesAsync();
+
+        await _scoreCalculator.Calculate(GetTime(59, 50));
+
+        var userScore = _mockContext.UserScores.Single(x => x.UserId == userId);
+        userScore.TotalScore.Should().Be(100 + (40 * 20 * 6) - (30 * 20 * 4));
+    }
+
+    [Test]
+    public async Task has_withdraw_after_last_update()
+    {
+        var userId = 1;
+        _mockContext.Users.AddRange(new User { UserId = userId });
+        _mockContext.UserScores.Add(new UserScore
+        {
+            UserId = userId,
+            TotalScore = 100,
+            LastUpdatedTime = GetTime(59, 10)
+        });
+        _mockContext.Assets.AddRange(new List<Asset>()
+        {
+            new()
+            {
+                UserId = userId,
+                TransactionType = TransactionType.Withdraw,
+                AssetType = AssetType.Banana,
+                Unit = 4,
+                CreatedTime = GetTime(59, 40)
+            }
+        });
+        _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Banana, Unit = 6 });
+
+        await _mockContext.SaveChangesAsync();
+
+        await _scoreCalculator.Calculate(GetTime(59, 50));
+
+        var userScore = _mockContext.UserScores.Single(x => x.UserId == userId);
+        userScore.TotalScore.Should().Be(100 + (40 * 20 * 6) + (30 * 20 * 4));
+    }
+
+
+    [Test]
+    public async Task has_different_type_deposit_after_last_update()
+    {
+        var userId = 1;
+        _mockContext.Users.AddRange(new User { UserId = userId });
+        _mockContext.UserScores.Add(new UserScore
+        {
+            UserId = userId,
+            TotalScore = 100,
+            LastUpdatedTime = GetTime(59, 10)
+        });
+        _mockContext.Assets.AddRange(new List<Asset>()
+        {
+            new()
+            {
+                AssetType = AssetType.Banana, Unit = 100, CreatedTime = GetTime(59, 40), UserId = userId,
+                TransactionType = TransactionType.Deposit
+            },
+            new()
+            {
+                AssetType = AssetType.Kiwi, Unit = 10, CreatedTime = GetTime(59, 40), UserId = userId,
+                TransactionType = TransactionType.Deposit
+            },
+            new()
+            {
+                AssetType = AssetType.Apple, Unit = 1, CreatedTime = GetTime(59, 40), UserId = userId,
+                TransactionType = TransactionType.Deposit
+            }
+        });
+        _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Banana, Unit = 110 });
+        _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Kiwi, Unit = 11 });
+        _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Apple, Unit = 2 });
+
+        await _mockContext.SaveChangesAsync();
+
+        await _scoreCalculator.Calculate(GetTime(59, 50));
+
+        var userScore = _mockContext.UserScores.Single(x => x.UserId == userId);
+        var expect = 100 + (110 * 20 * 40) + (11 * 10 * 40) + (2 * 100 * 40) -
+                     (100 * 20 * 30) - (10 * 10 * 30) - (1 * 100 * 30);
+        userScore.TotalScore.Should().Be(expect); // previous score + balance - diff
+    }
+
+
+    [Test]
+    public async Task two_user_has_deposit()
+    {
+        var userId = 1;
+        var userId2 = 2;
+        _mockContext.Users.AddRange(new User { UserId = userId }, new User() { UserId = userId2 });
+        _mockContext.UserScores.Add(new UserScore
+        {
+            UserId = userId,
+            TotalScore = 100,
+            LastUpdatedTime = GetTime(59, 10)
+        });
+        _mockContext.Assets.AddRange(new List<Asset>()
+        {
+            new()
+            {
+                UserId = userId, TransactionType = TransactionType.Deposit, AssetType = AssetType.Banana, Unit = 4,
+                CreatedTime = GetTime(59, 40)
+            },
+            new()
+            {
+                UserId = userId2, TransactionType = TransactionType.Deposit, AssetType = AssetType.Banana, Unit = 5,
+                CreatedTime = GetTime(59, 40)
+            },
+        });
+        _mockContext.Balances.Add(new Balance { UserId = userId, AssetType = AssetType.Banana, Unit = 6 });
+        _mockContext.Balances.Add(new Balance { UserId = userId2, AssetType = AssetType.Banana, Unit = 5 });
+
+        await _mockContext.SaveChangesAsync();
+
+        await _scoreCalculator.Calculate(GetTime(59, 50));
+
+        _mockContext.UserScores.Single(x => x.UserId == userId).TotalScore
+            .Should().Be(100 + (40 * 20 * 6) - (30 * 20 * 4));
+
+        _mockContext.UserScores.Single(x => x.UserId == userId2).TotalScore
+            .Should().Be((10 * 20 * 5) - 0); 
     }
 
     [TearDown]
