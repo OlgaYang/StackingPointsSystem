ec5298af638d652048860520163687dcdbfd850b
Olga <ya1994ng@gmail.com>
2024-08-26 23:14:22 +0800

    feat: completed Assets api

--- StakingPointsSystem/Controllers/UserController.cs
+++ StakingPointsSystem/Controllers/UserController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StakingPointsSystem.Models;
 
 namespace StakingPointsSystem.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]/[action]")]
 public class UserController : ControllerBase
 {
     private readonly StakingPointsDbContext _dbContext;
@@ -12,6 +13,21 @@ public class UserController : ControllerBase
     public UserController(StakingPointsDbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public async Task<AssetRequest> Assets(int userId)
+    {
+        var userScore = _dbContext.UserScores.FirstOrDefault(x => x.UserId == userId);
+        var score = userScore?.TotalScore ?? 0;
+
+        return new AssetRequest
+        {
+            Score = score,
+            Rank = _dbContext.UserScores.Count(x => x.TotalScore > score) + 1,
+            Balances = await _dbContext.Balances.Where(x => x.UserId == userId)
+                .ToDictionaryAsync(x => x.AssetType.ToString(), x => x.Unit)
+        };
     }
 
     [HttpGet]
@@ -26,13 +42,20 @@ public class UserController : ControllerBase
                 TransactionType = TransactionType.Deposit,
                 Unit = random.Next(0, 100),
                 CreatedTime = DateTime.Now,
-                AssetType = (AssetType)random.Next(1,4),
+                AssetType = (AssetType)random.Next(1, 4),
             };
-            
-            _dbContext.Assets.Add(asset); 
+
+            _dbContext.Assets.Add(asset);
         }
 
         await _dbContext.SaveChangesAsync();
         return _dbContext.Users.Select(x => x).ToList();
     }
+}
+
+public class AssetRequest
+{
+    public decimal Score { get; set; }
+    public int Rank { get; set; }
+    public Dictionary<string, decimal> Balances { get; set; }
 }
